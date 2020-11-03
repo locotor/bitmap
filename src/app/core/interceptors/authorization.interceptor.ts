06b47ac744b71b4c69bf0c42383f640ee6a8eeff
locotor <375522067@qq.com>
2020-11-03 17:48:20 +0800

    auth bug fixed

--- src/app/core/interceptors/authorization.interceptor.ts
+++ src/app/core/interceptors/authorization.interceptor.ts
@@ -11,9 +11,11 @@ import { Base64 } from 'js-base64';
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
 
+  auth = `Basic ${Base64.encode('admin:geoserver')}`
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const reqClone = request.clone({
-      setHeaders: { Authorization: 'Basic ' + Base64.encode('admin:geoserver') },
+      setHeaders: { Authorization: this.auth },
     });
     return next.handle(reqClone);
   }
