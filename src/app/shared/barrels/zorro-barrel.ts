aa80e7b511f24f18ad280a271824a80e9da424bb
locotor <375522067@qq.com>
2020-11-03 15:11:49 +0800

    simplify SharedModule

---
+++ src/app/shared/barrels/zorro-barrel.ts
@@ -0,0 +1,21 @@
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzCommentModule } from 'ng-zorro-antd/comment';
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+export const AntModules = [
+    NzAvatarModule,
+    NzIconModule,
+    NzDividerModule,
+    NzGridModule,
+    NzTagModule,
+    NzCommentModule,
+    NzMenuModule,
+    NzStepsModule,
+    NzTableModule
+];
---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,19 @@
+import { ThemePickerComponent } from './theme-picker/theme-picker.component';
+import { TaskListItemComponent } from './task-list-item/task-list-item.component';
+import { ProjectListItemComponent } from './project-list-item/project-list-item.component';
+import { TaskCardComponent } from './task-card/task-card.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { TeamCreatorComponent } from './team-creator/team-creator.component';
+import { ProjectCreatorComponent } from './project-creator/project-creator.component';
+
+export const SharedComponents = [
+    ThemePickerComponent,
+    TaskListItemComponent,
+    ProjectListItemComponent,
+    TaskCardComponent,
+    TaskDetailComponent,
+    AvatarComponent,
+    TeamCreatorComponent,
+    ProjectCreatorComponent
+]
