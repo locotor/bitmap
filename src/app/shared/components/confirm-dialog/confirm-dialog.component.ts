9577317bfcfa88298978c1febd07e7a376647b26
locotor <375522067@qq.com>
2020-11-05 10:15:58 +0800

    clean unuse component

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,19 +1,7 @@
 import { ThemePickerComponent } from './theme-picker/theme-picker.component';
-import { TaskListItemComponent } from './task-list-item/task-list-item.component';
-import { ProjectListItemComponent } from './project-list-item/project-list-item.component';
-import { TaskCardComponent } from './task-card/task-card.component';
-import { TaskDetailComponent } from './task-detail/task-detail.component';
-import { AvatarComponent } from './avatar/avatar.component';
-import { TeamCreatorComponent } from './team-creator/team-creator.component';
-import { ProjectCreatorComponent } from './project-creator/project-creator.component';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 export const SharedComponents = [
     ThemePickerComponent,
-    TaskListItemComponent,
-    ProjectListItemComponent,
-    TaskCardComponent,
-    TaskDetailComponent,
-    AvatarComponent,
-    TeamCreatorComponent,
-    ProjectCreatorComponent
+    ConfirmDialogComponent
 ]
